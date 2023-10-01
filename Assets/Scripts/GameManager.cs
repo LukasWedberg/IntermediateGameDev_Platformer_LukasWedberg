a135b972d620723203274749fc365d30d5171536
LukasWedberg <lew7761@nyu.edu>
2023-09-30 21:19:57 -0400

    Yup. This time I worked on the timer. Additionally, I refined the physics a bit more.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+
 
 public class GameManager : MonoBehaviour
 {
+    float timeAllowed = 180f;
+
+    [SerializeField]
+    TMP_Text timeRemainingUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Altering the timer
+        if (timeAllowed > 0f)
+        {
+            timeAllowed -= Time.deltaTime;
+
+            string extraZero = "";
+
+            if ( (Mathf.Round(timeAllowed) % 60)/10 < 1)
+            {
+                extraZero = "0";
+            }
+
+            timeRemainingUI.text = (Mathf.Ceil(timeAllowed/60)-1).ToString() +" : "+ extraZero + (Mathf.Round(timeAllowed)%60).ToString();
+
+        }
+
+
+
+        //Player energy depletion
+
+
+
     }
 }
